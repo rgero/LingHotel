3950ab3327d917797fe43811348639c7d5817c63
Roy Gero <roy.gero.ii@gmail.com>
2023-10-17 19:43:51 -0400

    Adding the tool provided by the course for data entry
    This tool and the data hashes are so I can go ahead and manipulate the data without worrying about having to re-enter everything. I've thrown it on the Stick Page so I can do some more work on the site in the future.

--- ling-hotel/src/pages/Stick.jsx
+++ ling-hotel/src/pages/Stick.jsx
@@ -1,9 +1,13 @@
+import Uploader from '../data/Uploader'
+
 const Stick = () => {
     return (
         <div>
             I&apos;m a stick!
             <br/>...
             <div>Why did you click me?</div>
+            
+            <Uploader/>
         </div>
     )
 }
