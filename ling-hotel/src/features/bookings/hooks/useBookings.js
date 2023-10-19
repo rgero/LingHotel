5fe6110cbcfe8feb20f50a560189af698bc9e286
Roy Gero <roy.gero.ii@gmail.com>
2023-10-18 21:49:06 -0400

    Adding sorting to the bookings.

--- ling-hotel/src/features/bookings/hooks/useBookings.js
+++ ling-hotel/src/features/bookings/hooks/useBookings.js
@@ -9,7 +9,13 @@ export const useBookings = () => {
   const filterValue = searchParams.get("status");
   const filter = !filterValue || filterValue === "all" ? null : {field: 'status', value: filterValue}
 
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = {field, direction}
+
+
   // This is cool, the filter input makes it a dependency like useEffect.
-  const {isLoading, data: bookings, error} = useQuery({queryKey: ["bookings", filter], queryFn: () => getBookings({filter})});
+  const {isLoading, data: bookings, error} = useQuery({queryKey: ["bookings", filter, sortBy], queryFn: () => getBookings({filter, sortBy})});
   return { isLoading, error, bookings };
 }
--- ling-hotel/src/services/apiBookings.js
+++ ling-hotel/src/services/apiBookings.js
@@ -5,9 +5,14 @@ export const getBookings = async ({filter, sortBy}) => {
     .from("bookings")
     .select("*, cabins(name), guests(fullName, email)");
 
-  if (filter !== null) 
+  if (filter) 
   {
     query = query.eq(filter.field, filter.value);
+  }
+
+  if (sortBy)
+  {
+    query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"})
   }
 
   const {data, error} = await query;
