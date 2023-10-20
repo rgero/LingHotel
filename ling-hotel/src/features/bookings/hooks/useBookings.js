fb6055f7b0fd7311324b82a1784a110c6589e62b
Roy Gero <roy.gero.ii@gmail.com>
2023-10-19 21:25:08 -0400

    Adding prefetching for pages surrounding the current one.

--- ling-hotel/src/features/bookings/hooks/useBookings.js
+++ ling-hotel/src/features/bookings/hooks/useBookings.js
@@ -1,8 +1,12 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { ENTRIES_PER_PAGE } from "../../../utils/constants";
 import { getBookings } from "../../../services/apiBookings";
-import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 export const useBookings = () => {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -19,6 +23,16 @@ export const useBookings = () => {
 
   // This is cool, the filter input makes it a dependency like useEffect.
   const {isLoading, data: {data: bookings, count} = {}, error} = useQuery({queryKey: ["bookings", filter, sortBy, page], queryFn: () => getBookings({filter, sortBy, page})});
+
+  // Pre-fetch some data
+  const pageCount = Math.ceil(count / ENTRIES_PER_PAGE);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({queryKey: ["bookings", filter, sortBy, page+1], queryFn: () => getBookings({filter, sortBy, page: page+1})})
+  }
+  if (page > 1)
+  {
+    queryClient.prefetchQuery({queryKey: ["bookings", filter, sortBy, page-1], queryFn: () => getBookings({filter, sortBy, page: page-1})})
+  }
 
   return { isLoading, error, bookings, count };
 }
