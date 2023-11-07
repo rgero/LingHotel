843c125582b325728e81d4e66a035b794d8cff36
Roy Gero <roy.gero.ii@gmail.com>
2023-11-06 21:03:36 -0500

    Adding the sign up process.
    This process would simulate a new employee being added. Therefore it's in a protected route because authenticated users can only create users

--- ling-hotel/src/features/authentication/SignupForm.jsx
+++ ling-hotel/src/features/authentication/SignupForm.jsx
@@ -1,35 +1,81 @@
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import Button from "../../styles/Button";
+import Form from "../../ui/forms/Form";
+import FormRow from "../../ui/forms/FormRow";
+import Input from "../../styles/Input";
+import { useForm } from "react-hook-form";
+import { useSignup } from "./hooks/useSignup";
 
-// Email regex: /\S+@\S+\.\S+/
+/*
+  Users are going to be employees of the hotel. Therefore the sign-up page should be in an authenticated route since public should not have access
+*/
 
 const SignupForm = () => {
+  const {signup, isLoading } = useSignup();
+  const {register, formState, getValues, handleSubmit, reset} = useForm()
+  const {errors} = formState;
+
+  const onSubmit = ({fullName, email, password}) => {
+    signup({ fullName, email, password}, {onSettled: reset} )
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Full name" errors={errors?.fullName?.message}>
+        <Input 
+          type="text" 
+          id="fullName"
+          disabled={isLoading}
+          {...register("fullName", {
+            required: "This field is required"
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label="Email address" errors={errors?.email?.message}>
+        <Input 
+          type="email" 
+          id="email"
+          disabled={isLoading}
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Please provide a valid email"
+            }
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow label="Password (min 8 characters)" errors={errors?.password?.message}>
+        <Input 
+          type="password" 
+          id="password"
+          disabled={isLoading}
+          {...register("password", {
+            required: "This field is required",
+            minLength: { value: 8, message: "Password must be at least 8 characters"}
+          })} 
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label="Repeat password" errors={errors?.passwordConfirm?.message}>
+        <Input 
+          type="password" 
+          id="passwordConfirm"
+          disabled={isLoading}
+          {...register("passwordConfirm", {
+            required: "This field is required",
+            validate: (value) => value === getValues().password || "Passwords much match"
+          })} 
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   )
--- ling-hotel/src/pages/Users.jsx
+++ ling-hotel/src/pages/Users.jsx
@@ -1,8 +1,12 @@
+import Heading from "../ui/Heading"
+import SignupForm from "../features/authentication/SignupForm"
+
 const Users = () => {
     return (
-        <div>
-            Users
-        </div>
+        <>
+            <Heading as="h1">Create a new user</Heading>
+            <SignupForm/>
+        </>
     )
 }
 
