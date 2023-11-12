4e3d75d361eac59174af17df6cb8e7808a4d4335
Roy Gero <roy.gero.ii@gmail.com>
2023-11-11 21:01:35 -0500

    Adding a way to prevent the blank screens.

--- ling-hotel/src/main.jsx
+++ ling-hotel/src/main.jsx
@@ -1,9 +1,13 @@
+import App from './App.jsx'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from './ui/ErrorFallback'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> window.location.replace("/")}>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
